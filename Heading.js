613627556fe80db5652cc245c01bd1806750c632
Gagandeep Singh <36220210+Gagan1804@users.noreply.github.com>
2020-11-12 22:43:16 +0530

    Add files via upload

---
+++ Heading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Heading = () => {
+    return (
+        <div>
+            <center>
+                <h1>Infinite scrolling react-app</h1>
+                <p style={{fontSize: "20px"}}>Using unsplash API</p>
+            </center>
+        </div>
+    )
+}
